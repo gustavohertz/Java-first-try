f94f5204131f59e163bb4fcd71287cb6d56e6062
Gustavo Borges Hertz <127430409+gustavohertz@users.noreply.github.com>
2023-07-31 08:16:07 -0300

    Create Math.java

---
+++ Math.java
@@ -0,0 +1,10 @@
+class Math {
+  public static void math(String[] args) {
+
+int numero1 = 30;
+int numero2 = 21;
+int soma = numero1+numero2;
+System.out.println("O valor da soma é: "+soma);
+  }
+}
+
